d02c3e6941ebb4c5c6fd80ce45c6a416763d7b6b
DarKS4ys <melihyardim1057@gmail.com>
2023-12-12 21:27:48 +0300

    a

--- components/ColorPicker.tsx
+++ components/ColorPicker.tsx
@@ -1,6 +1,6 @@
 "use client"
 import React, { useState } from 'react';
-import { GithubPicker, ColorResult, SwatchesPicker, CirclePicker } from 'react-color';
+import { ColorResult, CirclePicker } from 'react-color';
 import { Button } from './ui/button';
 import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from './ui/dropdown-menu';
 
