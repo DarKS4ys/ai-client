bf697c1ee2e67ffb40c29df64efa596466a5d84b
S-P-H-E <sphe.g.personal@gmail.com>
2023-12-14 20:17:39 +0200

    new prompt 500 words

--- lib/ai/prompt-templates.ts
+++ lib/ai/prompt-templates.ts
@@ -8,11 +8,9 @@ Standalone question:`;
 
 
 // Actual question you ask the chat and send the response to client
-export const QA_TEMPLATE = `when told to begin the summary, Summarize the current pdf. 
-Separate these three headings and its contents into paragraphs with three backticks before and after each paragraoh of information,
-for the opening set of backticks have a number after it eg. \\\`\\\`\\\`1 then have a new line the put the title of your summary. make sure to have three backticks at the end of each topic paragraph that you summarize like this \\\`\\\`\\\` then for the next summary begin with \\\`\\\`\\\`2 and so on
-Don't do any explaining just get to the point, and make sure to add the symbols mentioned above
-Do not include any information on photographs and make sure that inside of the backticks its just a heading then a paragraph which is the summary and dont use "-"
+export const QA_TEMPLATE = `when told to begin the summary
+summarise this document into 500 words and have multiple paragraphs
+
 {context}
 
 Question: {question}
